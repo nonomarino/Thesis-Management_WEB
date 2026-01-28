ef05b9122b4afefab450228e16c4508d8e981891
nonomarino <geomari147@gmail.com>
2026-01-28 19:14:29 +0200

    remove initial files

--- api/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-// api/logout.php
-session_start();
-session_destroy(); // Καθαρίζει τη μνήμη του Server
-header('Content-Type: application/json');
-echo json_encode(['status' => 'success']);
-?>
